8d7093c514ca1eeaa0127601bff439aa8d6c8885
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-11-30 23:25:05 +0530

    feat: trpc server setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import DarkLightContextProvider from "@/context/darkContext";
 import NavBar from "@/components/NavBar";
 import SideBar from "@/components/SideBar";
-
+import { trpc } from "../utils/trpc";
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   //  themes
   const [dark, setDark] = useState(true);
@@ -38,4 +38,4 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   );
 }
 
-export default MyApp;
+export default trpc.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import * as React from "react";
 import Layout from "@/components/layout/Layout";
 import Seo from "@/components/Seo";
+import { trpc } from "@/utils/trpc";
 
 export default function HomePage() {
+  const hello = trpc.hello.useQuery({ text: "client" });
+  if (!hello.data) {
+    return <div>Loading...</div>;
+  }
   return (
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
-
-      <main></main>
+      <main>
+        <p>{hello.data.greeting}</p>
+      </main>
     </Layout>
   );
 }
