75ac6de40bc78c168e6bf9c522ba6a3f846fc097
vidyesh-kumar <kumarvidyesh@gmail.com>
2022-12-04 10:47:14 +0530

    Tabs Added

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,7 +1,15 @@
+import { Tabs } from "flowbite-react";
 import React from "react";
 
 function resources() {
-  return <div>resources</div>;
+  const tabs = ['CSE', 'ISE', "MECH", "ECE", "EEE", "BT", "CIVIL", "AIML", "AIDS", "ROB", "MCA", "MTECH"];
+  const ele = tabs.map((tab) => { return <Tabs.Item title={tab} active={false} key="tab">{tab} Content</Tabs.Item> });
+  return <>
+    <h4>Select Department</h4>
+    <Tabs.Group className="flex flex-row items-start justify-around w-full">
+      {ele}
+    </Tabs.Group>
+  </>;
 }
 
 export default resources;
