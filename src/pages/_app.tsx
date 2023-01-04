23f2d5f580c4ad3c751064fa1370f3dc2dd6112b
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-01-04 12:45:32 +0530

    light color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,15 +24,15 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
           className={`${
             dark
               ? "dark bg-gradient-to-br from-slate-900 via-slate-800 to-gray-700 text-gray-200 "
-              : "bg-gradient-to-br from-sky-200 via-emerald-100 to-pink-50   text-gray-800 "
+              : "bg-gradient-to-br from-sky-100 via-emerald-100 to-pink-50   text-gray-800 "
           } relative h-screen  w-screen overflow-x-hidden overflow-y-hidden px-2 sm:px-5 `}>
           <NavBar />
           <div className=" mt-5 flex  h-[88vh] flex-col-reverse justify-between  gap-5 sm:mt-10 md:flex-row">
             <SideBar />
-            <div className="isolate mx-auto w-full max-w-7xl  space-y-5 overflow-y-auto scroll-smooth ">
+            <div className="isolate mx-auto h-full w-full  max-w-7xl space-y-5 overflow-y-auto scroll-smooth ">
               <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
                 <svg
-                  className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+                  className="relative left-[calc(50%-11rem)] -z-10  h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] opacity-40 dark:opacity-100 sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
                   viewBox="0 0 1155 678"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg">
@@ -58,7 +58,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
               <Component {...pageProps} dark={dark} setDark={setDark} />
               <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                 <svg
-                  className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+                  className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 opacity-5 dark:opacity-100 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
                   viewBox="0 0 1155 678"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg">
