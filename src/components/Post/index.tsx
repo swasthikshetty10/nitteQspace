aba0161e6b9ffe46aeced2c74d6372b19c9e212c
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-01-04 23:45:34 +0530

    fix:comment thread

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -2,7 +2,8 @@ import { Head } from "next/document";
 import Image from "next/image";
 import React, { useState } from "react";
 import { Carousel, Avatar, Badge } from "flowbite-react";
-import { BiComment, BiUpvote, BiDownvote, BiCommentAdd } from "react-icons/bi";
+import { BiComment, BiUpvote, BiDownvote } from "react-icons/bi";
+import { BsReply } from "react-icons/bs";
 import { ImArrowDown, ImArrowUp } from "react-icons/im";
 import { FaShare } from "react-icons/fa";
 import { HiClock, HiCode, HiOutlineSave, HiX } from "react-icons/hi";
@@ -312,6 +313,7 @@ const Comments = ({
 }) => {
   const comments = trpc.post.getComments.useQuery({ postId, threadId });
   const [show, showComments] = useState(true);
+  const [showBox, setShowBox] = useState(false);
   if (!comments.data) {
     return <div>Loading...</div>;
   }
@@ -321,8 +323,8 @@ const Comments = ({
         return (
           <div
             key={comment.id}
-            className="flex-flex-col min-w-fit  border-l-2 border-gray-900/20 p-5 dark:border-gray-100/20">
-            <div className="inline-flex w-full min-w-fit items-center  gap-3">
+            className="flex-flex-col min-w-fit gap-2  border-l-2 border-gray-900/20 p-5 pb-0 dark:border-gray-100/20">
+            <div className="-ml-4 inline-flex w-full min-w-fit items-center  gap-3">
               <div className="inline-flex items-center gap-2">
                 <Avatar
                   img={comment.author.image || undefined}
@@ -340,23 +342,34 @@ const Comments = ({
                 {getTimeInAMPM(new Date(comment.createdAt))}
               </Badge>
             </div>
-            <ThreadVotes threadId={comment.id} />
-
-            <div className="pb-3 ">
+            <div className=" border-l-2 border-gray-900/20 pl-5 pb-0 dark:border-gray-100/20">
               <div>
                 <p>{comment.content}</p>
               </div>
+              <div className="flex items-center gap-2">
+                <ThreadVotes threadId={comment.id} />
+                <button
+                  onClick={() => setShowBox((s) => !s)}
+                  className="my-3 flex items-center gap-1  opacity-80 transition-opacity hover:opacity-100">
+                  <BsReply className="text-xl sm:text-2xl" />
+                  <a className="hidden text-sm font-semibold sm:block">Reply</a>
+                </button>
+              </div>
+              <button
+                onClick={() => showComments((c) => !c)}
+                className="hidden  items-center gap-1 opacity-80 hover:opacity-100">
+                <BiComment className="text-xl sm:text-2xl" />
+                <a className="text-md hidden font-semibold sm:block">
+                  Comments
+                </a>
+                <a className="text-sm font-semibold text-gray-400"></a>
+              </button>
             </div>
-            <button
-              onClick={() => showComments((c) => !c)}
-              className="hidden  items-center gap-1 opacity-80 hover:opacity-100">
-              <BiComment className="text-xl sm:text-2xl" />
-              <a className="text-md hidden font-semibold sm:block">Comments</a>
-              <a className="text-sm font-semibold text-gray-400"></a>
-            </button>
             {show && (
               <>
-                <CommentBox threadId={comment.id} />
+                {showBox && (
+                  <CommentBox setShowBox={setShowBox} threadId={comment.id} />
+                )}
                 <Comments threadId={comment.id} />
               </>
             )}
@@ -370,11 +383,12 @@ const Comments = ({
 const CommentBox = ({
   threadId,
   postId,
+  setShowBox,
 }: {
   threadId?: number;
   postId?: number;
+  setShowBox?: (show: boolean) => void;
 }) => {
-  const [show, setShow] = useState(false);
   const [showGif, setShowGif] = useState(false);
   const [comment, setComment] = useState("");
   const [gif, setGif] = useState("");
@@ -382,7 +396,7 @@ const CommentBox = ({
   const utils = trpc.useContext();
   const mutation = trpc.post.addComment.useMutation({
     onSuccess: () => {
-      setShow(false);
+      setShowBox && setShowBox(false);
       setComment("");
       setGif("");
       setImages([]);
@@ -391,9 +405,6 @@ const CommentBox = ({
   });
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    console.log(comment);
-    console.log(gif);
-    console.log(images);
     mutation.mutate({
       threadId: threadId,
       content: comment,
@@ -403,103 +414,87 @@ const CommentBox = ({
 
   return (
     <>
-      {!show ? (
-        <button
-          onClick={() => setShow(true)}
-          className="my-3 flex items-center gap-1 opacity-80 hover:opacity-100">
-          <BiCommentAdd className="text-xl sm:text-2xl" />
-          <a className="text-md hidden font-semibold sm:block">Add Comment</a>
-        </button>
-      ) : (
-        <>
-          <form onSubmit={handleSubmit} className="relative mt-3">
+      <form onSubmit={handleSubmit} className="relative mt-3">
+        <div className="mb-4 w-full rounded-lg border border-gray-200 bg-slate-100/80  dark:border-gray-600 dark:bg-gray-700">
+          <div className="rounded-t-lg bg-slate-100/80 px-4 py-2 dark:bg-gray-800">
+            <label htmlFor="comment" className="sr-only">
+              Your comment
+            </label>
+            <textarea
+              id="comment"
+              onChange={(e) => setComment(e.target.value)}
+              value={comment}
+              rows={4}
+              className="w-full border-0 bg-slate-200/50 p-2  text-sm text-gray-900 focus:ring-0 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400"
+              placeholder="Write a comment..."
+              required></textarea>
+          </div>
+          <div className="flex items-center justify-between border-t px-3 py-2 dark:border-gray-600">
             <button
-              onClick={() => setShow(false)}
-              className="absolute top-3 right-2 p-2 transition-all hover:scale-110">
-              <AiOutlineClose />
+              type="submit"
+              className="hi inline-flex items-center rounded-lg bg-slate-700/60 py-2.5 px-4 text-center text-xs font-medium text-white hover:bg-slate-700 focus:ring-4 focus:ring-slate-200 dark:bg-slate-200 dark:text-slate-700 dark:hover:bg-slate-50 dark:focus:ring-slate-900">
+              Post comment
             </button>
-            <div className="mb-4 w-full rounded-lg border border-gray-200 bg-slate-100/80  dark:border-gray-600 dark:bg-gray-700">
-              <div className="rounded-t-lg bg-slate-100/80 px-4 py-2 dark:bg-gray-800">
-                <label htmlFor="comment" className="sr-only">
-                  Your comment
-                </label>
-                <textarea
-                  id="comment"
-                  onChange={(e) => setComment(e.target.value)}
-                  value={comment}
-                  rows={4}
-                  className="w-full border-0 bg-slate-200/50 p-2  text-sm text-gray-900 focus:ring-0 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400"
-                  placeholder="Write a comment..."
-                  required></textarea>
-              </div>
-              <div className="flex items-center justify-between border-t px-3 py-2 dark:border-gray-600">
-                <button
-                  type="submit"
-                  className="hi inline-flex items-center rounded-lg bg-slate-700/60 py-2.5 px-4 text-center text-xs font-medium text-white hover:bg-slate-700 focus:ring-4 focus:ring-slate-200 dark:bg-slate-200 dark:text-slate-700 dark:hover:bg-slate-50 dark:focus:ring-slate-900">
-                  Post comment
-                </button>
 
-                <div className="flex space-x-1 pl-0 text-xl sm:pl-2">
-                  <button
-                    type="button"
-                    className="inline-flex cursor-pointer justify-center rounded p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white">
-                    <svg
-                      aria-hidden="true"
-                      className="h-5 w-5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        fill-rule="evenodd"
-                        d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
-                        clip-rule="evenodd"></path>
-                    </svg>
-                    <span className="sr-only">Attach file</span>
-                  </button>
-                  <button
-                    type="button"
-                    className="inline-flex cursor-pointer justify-center rounded p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white">
-                    <HiCode />
-                    <span className="sr-only">Code</span>
-                  </button>
-                  <button
-                    onClick={() => setShowGif(true)}
-                    type="button"
-                    className="inline-flex cursor-pointer justify-center rounded p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white">
-                    <MdOutlineGif className="-m-2 text-4xl" />
-                    <span className="sr-only">Upload image</span>
-                  </button>
-                </div>
-              </div>
-              {showGif && (
-                <div className="flex w-full justify-end gap-2 p-2">
-                  <Tenor
-                    token="PFOXR5L7C95P"
-                    onSelect={(result) => console.log(result)}
-                  />
-
-                  <button
-                    onClick={() => setShowGif(false)}
-                    type="button"
-                    className="inline-flex cursor-pointer justify-center rounded p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white">
-                    <HiX />
-                    <span className="sr-only">Close</span>
-                  </button>
-                </div>
-              )}
+            <div className="flex space-x-1 pl-0 text-xl sm:pl-2">
+              <button
+                type="button"
+                className="inline-flex cursor-pointer justify-center rounded p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white">
+                <svg
+                  aria-hidden="true"
+                  className="h-5 w-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
+                    clip-rule="evenodd"></path>
+                </svg>
+                <span className="sr-only">Attach file</span>
+              </button>
+              <button
+                type="button"
+                className="inline-flex cursor-pointer justify-center rounded p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white">
+                <HiCode />
+                <span className="sr-only">Code</span>
+              </button>
+              <button
+                onClick={() => setShowGif(true)}
+                type="button"
+                className="inline-flex cursor-pointer justify-center rounded p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white">
+                <MdOutlineGif className="-m-2 text-4xl" />
+                <span className="sr-only">Upload image</span>
+              </button>
             </div>
-          </form>
-          <p className="ml-auto text-xs text-gray-500 dark:text-gray-400">
-            Remember, contributions to this topic should follow our{" "}
-            <a
-              href="#"
-              className="text-blue-600 hover:underline dark:text-blue-500">
-              Community Guidelines
-            </a>
-            .
-          </p>
-        </>
-      )}
+          </div>
+          {showGif && (
+            <div className="flex w-full justify-end gap-2 p-2">
+              <Tenor
+                token="PFOXR5L7C95P"
+                onSelect={(result) => console.log(result)}
+              />
+
+              <button
+                onClick={() => setShowGif(false)}
+                type="button"
+                className="inline-flex cursor-pointer justify-center rounded p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white">
+                <HiX />
+                <span className="sr-only">Close</span>
+              </button>
+            </div>
+          )}
+        </div>
+      </form>
+      <p className="ml-auto text-xs text-gray-500 dark:text-gray-400">
+        Remember, contributions to this topic should follow our{" "}
+        <a
+          href="#"
+          className="text-blue-600 hover:underline dark:text-blue-500">
+          Community Guidelines
+        </a>
+        .
+      </p>
     </>
   );
 };
